1a98296dc6ef4c0e709c7a7a48e0f30108e291c0
Alexis Tamayo <lexx426@gmail.com>
2024-01-18 16:08:51 -0600

    Completed the rating portion for both movies and tv shows.

--- src/pages/home/column_display.tsx
+++ src/pages/home/column_display.tsx
@@ -1,6 +1,9 @@
-import { Card, Grid } from "semantic-ui-react";
+import { Card, Form, FormField, FormInput, Grid } from "semantic-ui-react";
 import { DisplayType } from "./home";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { rateMovie, rateTvShow } from "../rated/rate_mutation";
 
 interface DisplayData{
     id: number;
@@ -10,6 +13,7 @@ interface DisplayData{
     name?: string;
     vote_average: number;
     release_date: string;
+
 }
 
 interface Props {
@@ -18,8 +22,22 @@ interface Props {
 }
 
 export const ColumnDisplay = (props: Props) =>{
-    const {data, displayType} = props
+    const {data, displayType} = props;
 
+    const [rating, setRating] = useState<number>(0);
+
+    const { mutate: rateMovieMutation} =useMutation({
+        mutationKey: ["rateMovie"], 
+        mutationFn: (id: number) => rateMovie(id, rating),
+    });
+
+    const { mutate: rateTvShowMutation} =useMutation({
+        mutationKey: ["rateTvShow"], 
+        mutationFn: (id: number) => rateTvShow(id, rating),
+    });
+
+    const rate = displayType === DisplayType.Movies ? rateMovieMutation : rateTvShowMutation;
+    
     return (
         <Grid
             columns={3}
@@ -37,14 +55,35 @@ export const ColumnDisplay = (props: Props) =>{
                                 image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`}
                                 header={ displayType === DisplayType.Movies
                                     ? displayData.title
-                                    : displayData.name} 
+                                    : displayData.name
+                                } 
                                 meta={`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`} 
                                 description={displayData.overview.slice(0,350) + "..."}
-                            />
+                            /> {" "}
                         </Link>
+                        <Form style={{marginTop: 10}}>
+                            <Form.Group inline>
+                                <Form.Field>
+                                    <Form.Input 
+                                        type="number" 
+                                        min="0" 
+                                        max="10" 
+                                        step="0.5" 
+                                        onChange={(e) => setRating(Number(e.target.value))} 
+                                        action={{
+                                            color:"violet", 
+                                            labelPosition:"right", 
+                                            icon:"star", 
+                                            content: "Rate", 
+                                            onClick: () => rate(displayData.id),
+                                        }}
+                                    />
+                                </Form.Field>
+                            </Form.Group>
+                        </Form>
                     </Card.Group>
                 </Grid.Column>
             ))}
         </Grid>
-    )
-}
+    );
+};
